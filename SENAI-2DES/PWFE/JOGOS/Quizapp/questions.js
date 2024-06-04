7fe68fcbadc67a1c0c2fdec294c93f7df0cd8b7b
lehhofman <desenvolvimentoleticia43@gmail.com>
2024-06-04 09:03:15 -0300

    Projeto

---
+++ SENAI-2DES/PWFE/JOGOS/Quizapp/questions.js
@@ -0,0 +1,99 @@
+export default [
+    {
+        question: "Qual é o principal ingrediente do prato feijoada?",
+        answers: [
+            {option: "Feijão", correct:false},
+            {option: "Carne de porco", correct:false},
+            {option: "Feijão preto", correct:true},
+        ],
+    },
+
+    {
+        question: "Qual é o nome do prato típico da região nordeste do Brasil feito à base de carne de sol, farinha de mandioca e temperos?",
+        answers: [
+            {option: "Buchada de bode", correct:false},
+            {option: "Carne de sol com mandioca", correct:false},
+            {option: "Baião de dois", correct:true},
+        ],
+    },
+    
+    {
+        question: "Qual é a fruta principal do doce conhecido como 'cajuzinho'?",
+        answers: [
+            {option: "Morango", correct:false},
+            {option: "Coco", correct:false},
+            {option: "Caju", correct:true},
+        ],
+        
+    },
+
+    {
+        question: "O que é o acarajé?",
+        answers: [
+            {option: "Um tipo de peixe frito", correct:false},
+            {option: "Um tipo de torta de frango", correct:false},
+            {option: "Um bolinho de feijão fradinho frito em azeite de dendê, típico da culinária baiana", correct:true},
+        ],
+        
+    },
+
+    {
+        question: "Qual é o nome do prato típico da culinária paraense feito com peixe, camarão, leite de coco, azeite de dendê, pimenta e outros temperos?",
+        answers: [
+            {option: "Moqueca", correct:true},
+            {option: "Vatapá", correct:false},
+            {option: "Tacacá", correct:false},
+        ],
+        
+    },
+
+    {
+        question: "Qual é o nome do prato típico gaúcho que consiste em uma churrasqueira de metal com uma grelha móvel, na qual são assados diversos tipos de carne?",
+        answers: [
+            {option: "Parrilla", correct:false},
+            {option: "Churrasco", correct:false},
+            {option: "Churrasqueira", correct:true},
+        ],
+        
+    },
+
+    {
+        question: "Qual é o ingrediente principal do prato 'moqueca capixaba'?",
+        answers: [
+            {option: "Peixe", correct:true},
+            {option: "Carne de porco", correct:false},
+            {option: "Frango", correct:false},
+        ],
+        
+    },
+
+    {
+        question: "Qual é o nome do prato típico da culinária mineira feito com feijão tropeiro, bacon, linguiça, ovos, cebola, alho e temperos?",
+        answers: [
+            {option: "Tutu de feijão", correct:false},
+            {option: "Feijoada", correct:false},
+            {option: "Feijão tropeiro", correct:true},
+        ],
+        
+    },
+
+    {
+        question: "Qual é o principal ingrediente do doce típico de festas juninas chamado 'pé de moleque'?",
+        answers: [
+            {option: "Milho", correct:false},
+            {option: "Amendoim", correct:true},
+            {option: "Coco", correct:false},
+        ],
+        
+    },
+
+    {
+        question: "Qual é o nome do prato típico do estado da Bahia que consiste em uma sopa de origem africana feita com camarão seco, peixe, azeite de dendê, leite de coco, pimentão, cebola, tomate e temperos?",
+        answers: [
+            {option: "Acarajé", correct:false},
+            {option: "Moqueca", correct:false},
+            {option: "Abara", correct:true},
+        ],
+        
+    },
+];
