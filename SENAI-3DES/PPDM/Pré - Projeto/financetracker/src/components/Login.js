0059f16e222923b909ea4b6447ead531aab3d8b7
lehhofman <desenvolvimentoleticia2106@gmail.com>
2024-07-26 09:44:50 -0300

    Projeto

--- SENAI-3DES/PPDM/Pré - Projeto/financetracker/src/components/Login.js
+++ SENAI-3DES/PPDM/Pré - Projeto/financetracker/src/components/Login.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 
-const Login = ({ onLogin }) => {
+const Login = ({ onLogin, onRegister }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [isRegistering, setIsRegistering] = useState(false);
 
-  // Função para verificar se o usuário está registrado
-  const isUserRegistered = (email, password) => {
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-    return users.some(user => user.email === email && user.password === password);
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      if (e.target.name === 'email') {
+       document.getElementById('passwordInput').focus();
+      } else if (e.target.name === 'password') {
+         handleLogin();
+      }
+    }
   };
 
   const handleLogin = () => {
-    if (isUserRegistered(email, password)) {
+    if (email === 'user@example.com' && password === 'password') {
       onLogin(true);
       setError('');
     } else {
@@ -23,15 +27,10 @@ const Login = ({ onLogin }) => {
 
   const handleRegister = () => {
     if (email && password) {
-      // Adiciona o novo usuário ao localStorage
-      const newUser = { email, password };
-      const users = JSON.parse(localStorage.getItem('users')) || [];
-      users.push(newUser);
-      localStorage.setItem('users', JSON.stringify(users));
-      
-      // Após o registro bem-sucedido, automaticamente faz login
-      onLogin(true);
+      onRegister({ name: 'Novo Usuário', email, password });
+      setIsRegistering(false);
       setError('');
+      onLogin(true);
     } else {
       setError('Por favor, preencha todos os campos.');
     }
@@ -48,14 +47,19 @@ const Login = ({ onLogin }) => {
       <input
         type="email"
         placeholder="Email"
+        name="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
+        onKeyPress={handleKeyPress}
       />
       <input
         type="password"
         placeholder="Password"
+        name="password"
+        id="passwordInput"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
+        onKeyPress={handleKeyPress}
       />
       {error && <p style={{ color: 'red' }}>{error}</p>}
       {isRegistering ? (
