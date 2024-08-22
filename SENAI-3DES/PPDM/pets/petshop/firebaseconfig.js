da10a06a297c652d45f94e3b4fb5963971afc04c
lehhofman <desenvolvimentoleticia2106@gmail.com>
2024-08-22 10:11:32 -0300

    proj

---
+++ SENAI-3DES/PPDM/pets/petshop/firebaseconfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZR9Cuvj2t1cCLoEDV-rJ-e-XAaoAu5fU",
+  authDomain: "petshop-31971.firebaseapp.com",
+  projectId: "petshop-31971",
+  storageBucket: "petshop-31971.appspot.com",
+  messagingSenderId: "996790857108",
+  appId: "1:996790857108:web:d20c6260d3e810cd297381"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+const auth = getAuth(app);
+
+export { db, auth };
